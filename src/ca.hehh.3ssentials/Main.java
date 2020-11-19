abcdf8d9c2d88f2faa260e9d94d6f265bfff0c24
hehh <58263307+itsmehehh@users.noreply.github.com>
2020-11-19 10:45:53 -0500

    Update Main.java

--- src/ca.hehh.3ssentials/Main.java
+++ src/ca.hehh.3ssentials/Main.java
@@ -1,6 +1,15 @@
 public class Main extends JavaPlugin;
 
+import Override;
 
+@Override
 public void onEnable(){
+  System.out.println("Plugin 3ssentials is now enabled.")
   
+  getCommand("help").setExecutor(new Commands);
+  
+}
+
+public void onDisable(){
+  System.out.println("Plugin 3ssentials is now disabled.")
 }
